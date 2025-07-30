ba2b03cb627e662b081c2b6084abdf65d11b2731
Hammed <salimonuh@gmail.com>
2025-07-30 16:02:39 +0100

    qmed_web_solutions_first_commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+// import reactLogo from './assets/react.svg';
+// import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+  const [test, setTest] = useState();
+  return (
+    <>
+      <p>qmed web solutions</p>
+    </>
+  );
+}
+
+export default App;
